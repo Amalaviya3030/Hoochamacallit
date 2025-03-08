80dc1f82961846c75b362aebf04eb9afe1a6e79d
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-03-08 16:08:11 -0500

    queues added

--- dr.c
+++ dr.c
@@ -1,2 +1,20 @@
-//dr.c
-//commit
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+#define SHM_KEY 16535
+#define MSG_KEY 12345
+
+struct message {
+    long msgType;
+    int dcID;
+    char data[100];
+};
+
+typedef struct {
+    int msgQueueID;
+    int numDCs;
+    int dcList[10];
+} MasterList;
