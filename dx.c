8656fb90ff0c1da4840754288b855d4ec0572a46
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-03-08 17:46:46 -0500

    dx is attached to memory and logging is implemented

--- dx.c
+++ dx.c
@@ -23,3 +23,21 @@ void log_event(const char* message) {
         fclose(logFile);
     }
 }
+
+int main() {
+    log_event("DX started. Trying to attach to shared memory...");
+
+    int shmID = shmget(SHM_KEY, sizeof(MasterList), 0666);
+    if (shmID == -1) {
+        perror("shmget failed (is DR running?)");
+        exit(1);
+    }
+
+    log_event("Shared memory found. Attaching...");
+    MasterList* master = (MasterList*)shmat(shmID, NULL, 0);
+    if (master == (void*)-1) {
+        perror("shmat failed");
+        exit(1);
+    }
+
+    log_event("Successfully attached to shared memory.");
