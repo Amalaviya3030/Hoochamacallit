4ff1afa1c09a6deaff3ce64e51b1c955ff721419
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-03-08 19:48:37 -0500

    implemented the logic to recieve the messages

--- dc.c
+++ dc.c
@@ -1,1 +1,24 @@
-//commit
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MSG_KEY 12345
+
+struct message {
+    long msgType;
+    int dcID;
+    char data[100];
+};
+
+int main() {
+    int msgQueueID;
+    struct message msg;
+
+    msgQueueID = msgget(MSG_KEY, 0666);
+    if (msgQueueID == -1) {
+        perror("msgget failed");
+        exit(1);
+    }
